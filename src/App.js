8d064d0bd7e5ad4854b209c4207a3a3d44dedbc6
georgewakhu270 <georgewakhu270@gmail.com>
2025-11-12 08:46:22 +0000

    build the first

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="bg-sky-500">Hello world</div>
+      <div className=""></div>
     </div>
   );
 }
